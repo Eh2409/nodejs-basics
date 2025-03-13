ff398c7afb93f86343241c05ddc47c6afb72b7fe
eliad <eliad.haim@gmail.com>
2025-03-13 18:17:08 +0200

    Improve about time

--- about-time.js
+++ about-time.js
@@ -2,16 +2,10 @@ import fs from 'fs'
 import ms from 'ms'
 
 readTimeFile()
-    .then(res => {
-        res.split('\r\n').forEach(time => console.log(ms(+time, { long: true })))
-    })
-    .catch(error => console.log(error))
 
 function readTimeFile() {
-    return new Promise((resolve, reject) => {
-        fs.readFile('data/time.txt', 'utf8', (err, content) => {
-            if (err) return reject(err)
-            resolve(content)
-        })
+    fs.readFile('data/time.txt', 'utf8', (err, content) => {
+        if (err) return reject(err)
+        content.split('\r\n').forEach(time => console.log(ms(+time, { long: true })))
     })
 }
